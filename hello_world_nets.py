6b42265e64bbac5f189a9464023512e7bc9a4d55
doola-x <jackdulla47@gmail.com>
2023-12-09 16:02:01 -0600

    Initial commit

---
+++ hello_world_nets.py
@@ -0,0 +1,54 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+# Load the MNIST dataset
+train_set = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
+
+test_set = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=1000, shuffle=False)
+
+# Define a simple neural network
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = torch.nn.Linear(28 * 28, 500)
+        self.fc2 = torch.nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+net = Net()
+
+# Define loss function and optimizer
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+
+# Train the network
+for epoch in range(2):  # loop over the dataset multiple times
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+print('Finished Training')
+
+# Test the network on the test data
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in test_loader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
