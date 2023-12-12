3bbb3dc43001b06b4a6809b4436e2677b35d72e1
doola-x <jackdulla47@gmail.com>
2023-12-11 22:32:07 -0600

    model now at 97.63% correctness :)

--- hello_world_nets.py
+++ hello_world_nets.py
@@ -1,30 +1,23 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
 # Load the MNIST dataset
 train_set = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
 
 test_set = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=1000, shuffle=False)
-
-# Get some random training images
-dataiter = iter(train_loader)
-images, labels = next(dataiter)
-
-# Show tensor values for the first image in the batch
-print(images[0])
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=10000, shuffle=False)
 
 # Define a simple neural network
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = torch.nn.Linear(28 * 28, 16)
-        self.fc2 = torch.nn.Linear(16, 16)
-        self.fc3 = torch.nn.Linear(16, 10)
+        self.fc1 = torch.nn.Linear(28 * 28, 100, bias=False)
+        self.fc2 = torch.nn.Linear(100, 100, bias=True)
+        self.fc3 = torch.nn.Linear(100, 10, bias=True)
 
     def forward(self, x):
         x = x.view(-1, 28 * 28)
@@ -39,8 +32,23 @@ net = Net()
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(net.parameters(), lr=0.1, momentum=0.9)
 
+number_of_epochs = 10
 # Train the network
-for epoch in range(2):  # loop over the dataset multiple times
+for epoch in range(number_of_epochs):  # loop over the dataset multiple times
+    running_loss = 0.0
+    # Wrap your data loader with tqdm for a progress bar
+    for i, data in tqdm(enumerate(train_loader, 0), total=len(train_loader), desc=f"Epoch {epoch+1}/{number_of_epochs}"):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+    
+    print(f"Epoch {epoch + 1} completed, Loss: {running_loss / len(train_loader)}")
+
     for i, data in enumerate(train_loader, 0):
         inputs, labels = data
         optimizer.zero_grad()
@@ -50,20 +58,6 @@ for epoch in range(2):  # loop over the dataset multiple times
         optimizer.step()
 
 print('Finished Training')
-
-def visualize_weights(layer):
-    with torch.no_grad():
-        weights = layer.weight.data
-        #weights = weights.view(-1, 28, 28)  # Reshape for MNIST 28x28 images
-
-        fig, axes = plt.subplots(10, 10, figsize=(10, 10))  # Adjust the subplot layout and size
-        for i, ax in enumerate(axes.flat):
-            if i < weights.shape[0]:
-                ax.imshow(weights[i], cmap='gray')
-                ax.axis('off')
-        plt.show()
-
-visualize_weights(net.fc1)
 
 # Test the network on the test data
 correct = 0
@@ -76,4 +70,4 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+print(f'Accuracy of the network on the 10000 test images: {100.00 * correct / total} %')
